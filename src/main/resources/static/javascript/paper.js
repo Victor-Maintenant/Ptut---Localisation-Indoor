3db431d472d6a5924dd18c8ad8a70aa4b0f6c4cf
Beckersl <beckerslucas1@gmail.com>
2021-04-19 23:46:19 +0200

    implémentation démo itinéraire v2

---
+++ src/main/resources/static/javascript/paper.js
@@ -0,0 +1,26 @@
+var path = new Path();
+
+var myCircle0 = new Path.Circle(new Point(1230, 800), 15);
+myCircle0.fillColor = 'yellow';
+var myCircle1 = new Path.Circle(new Point(1130, 870), 15);
+myCircle1.fillColor = 'blue';
+var myCircle2 = new Path.Circle(new Point(1300, 850), 15);
+myCircle2.fillColor = 'red';
+var myCircle3 = new Path.Circle(new Point(1300, 930), 15);
+myCircle3.fillColor = 'pink';
+var myCircle4 = new Path.Circle(new Point(1380, 840), 15);
+myCircle4.fillColor = 'green';
+var myCircle5 = new Path.Circle(new Point(1380, 920), 15);
+myCircle5.fillColor = 'purple';
+var myCircle6 = new Path.Circle(new Point(1440, 820), 15);
+myCircle6.fillColor = 'orange';
+var myCircle7 = new Path.Circle(new Point(1450, 900), 15);
+myCircle7.fillColor = 'grey';
+var myCircle8 = new Path.Circle(new Point(1540, 800), 15);
+myCircle8.fillColor = 'cyan';
+var myCircle9 = new Path.Circle(new Point(1590, 870), 15);
+myCircle9.fillColor = 'black';
+
+
+
+
