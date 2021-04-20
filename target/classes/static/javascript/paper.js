1e8c8399b2587e4105bd09228f4eec0993b9487b
Victor <victor.maintenant@etud.univ-jfc.fr>
2021-04-20 15:21:27 +0200

    fix affichage plan serveur

---
+++ target/classes/static/javascript/paper.js
@@ -0,0 +1,26 @@
+var path = new Path();
+
+var myCircle0 = new Path.Circle(new Point(1230, 700), 15);
+myCircle0.fillColor = 'yellow';
+var myCircle1 = new Path.Circle(new Point(1130, 770), 15);
+myCircle1.fillColor = 'blue';
+var myCircle2 = new Path.Circle(new Point(1300, 750), 15);
+myCircle2.fillColor = 'red';
+var myCircle3 = new Path.Circle(new Point(1300, 830), 15);
+myCircle3.fillColor = 'pink';
+var myCircle4 = new Path.Circle(new Point(1380, 740), 15);
+myCircle4.fillColor = 'green';
+var myCircle5 = new Path.Circle(new Point(1380, 820), 15);
+myCircle5.fillColor = 'purple';
+var myCircle6 = new Path.Circle(new Point(1440, 720), 15);
+myCircle6.fillColor = 'orange';
+var myCircle7 = new Path.Circle(new Point(1450, 800), 15);
+myCircle7.fillColor = 'grey';
+var myCircle8 = new Path.Circle(new Point(1540, 700), 15);
+myCircle8.fillColor = 'cyan';
+var myCircle9 = new Path.Circle(new Point(1590, 770), 15);
+myCircle9.fillColor = 'black';
+
+
+
+
